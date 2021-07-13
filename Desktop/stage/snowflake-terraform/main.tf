8992c382350b64f4bb0508ed8b23c0a6247b7900
Naima Houbati <naimahoubati93@gmail.com>
2021-07-13 18:33:58 -0400

    Add Service User, Schema, Grants

--- Desktop/stage/snowflake-terraform/main.tf
+++ Desktop/stage/snowflake-terraform/main.tf
@@ -1,15 +1,19 @@
 terraform {
+  required_version = ">= 0.13.0"
   required_providers {
     snowflake = {
       source  = "chanzuckerberg/snowflake"
-      version = "0.22.0"
+      version = "0.25.10"
     }
   }
 }
 
 provider "snowflake" {
+  region = "us-west-2"
   role     = "SYSADMIN"
+  password = "Naima1993"
 }
+
 
 resource "snowflake_database" "db" {
   name = "TF_DEMO"
